86c44d0fcf1d8c5fd325f3ac7e2851cb5d661482
cmthomas000 <cmthomas000@users.noreply.github.com>
2020-01-07 22:08:42 -0700

    Firmware chickenscratch

---
+++ FIFO_v1_0.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/07/2020 10:04:31 PM
+// Design Name: 
+// Module Name: FIFO_v1_0
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module FIFO_v1_0(
+        input wire AUDIO_DO,
+        input wire clk,
+        output wire dataReady,
+        output wire [7:0] dataOut
+    );
+    
+    
+    
+    
+    
+endmodule
